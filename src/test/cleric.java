3abda85cdc439af31538448e1ba196747be9afcd
kazutotaniyoshi <it352032@gmail.com>
2022-03-05 18:46:35 +0900

    練習

--- src/test/cleric.java
+++ src/test/cleric.java
@@ -1,10 +1,18 @@
 package test;
 
 public class cleric {
+String name;
+int hp =50;
+int maxHp =50;
+int mp =10;
+int maxMp=10;
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+public void selfAid() {
+	System.out.println("聖職者はセルフエイドを唱えた");
+	this.hp =this.maxHp;
+	this.mp -= 5;
+	System.out.println("HPが全快になった。");
+}
 
-	}
 
 }
